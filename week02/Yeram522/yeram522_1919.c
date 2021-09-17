8088d588b7462a2134d3c6c280ea5770d0870019
Marey <63442636+Yeram522@users.noreply.github.com>
2021-09-17 11:49:43 +0900

    [09/17] 1919 수정
    재혁쓰 도움으로...정답!!!!예에에에에🎉

--- week02/Yeram522/yeram522_1919.c
+++ week02/Yeram522/yeram522_1919.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define ALPHA 26
 int main()
 {
 	char wor1[1000], wor2[1000];
-	char arr1[26] = { 0, }, arr2[26] = { 0, };
+	char arr1[ALPHA] = { 0, }, arr2[ALPHA] = { 0, };
 	int index = 0,result = 0;
 
 	//ют╥б
@@ -15,11 +16,16 @@ int main()
 	while (wor1[index] != '\0')
 	{
 		arr1[(int)wor1[index] - 97] += 1;
+		index++;
+	}
+	index = 0;
+	while (wor2[index] != '\0')
+	{
 		arr2[(int)wor2[index] - 97] += 1;
 		index++;
 	}
-
-	for(int i = 0; i<26;i++)  result += abs(arr1[i] - arr2[i]);
+	
+	for(int i = 0; i< ALPHA ;i++)  result += abs(arr1[i] - arr2[i]);
 
 	printf("%d", result);	
 
