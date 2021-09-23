f413187b94fd4c637e495c2aae8d99848f4d1def
Marey <63442636+Yeram522@users.noreply.github.com>
2021-09-23 17:12:28 +0900

    21.09.23_17478_재귀함수가 뭔가요?_완료✅

---
+++ week03/Yeram522/yeram522_17478.c
@@ -0,0 +1,53 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+
+
+void print_line(int _count)
+{
+	for (int i = 0; i < _count; i++)
+		printf("____");
+
+	return;
+}
+
+
+void Chat_Bot(int _count,int _num)
+{
+	printf("\"재귀함수가 뭔가요?\"\n");
+
+	if (_count == 0)
+	{
+		print_line(_num);
+		printf("\"재귀함수는 자기 자신을 호출하는 함수라네\"\n");
+		print_line(_num);
+		printf("라고 답변하였지.\n");
+		return;		
+	}
+	else
+	{
+		print_line(_num);
+		printf("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n");
+		print_line(_num);
+		printf("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n");
+		print_line(_num);
+		printf("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n");
+	}
+
+	_count--;
+	_num++;
+	print_line(_num);
+	Chat_Bot(_count, _num);
+	print_line(_num-1);
+	printf("라고 답변하였지.\n");	
+}
+
+int main()
+{
+	int count;
+	scanf("%d", &count,0);
+
+	printf("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n");
+	Chat_Bot(count,0);
+}
