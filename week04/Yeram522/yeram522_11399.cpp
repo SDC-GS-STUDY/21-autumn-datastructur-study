483df3a443452ed8f3cfb501b440aca7c1f6c6b9
Marey <63442636+Yeram522@users.noreply.github.com>
2021-10-01 18:44:13 +0900

    🕕실행시간 측정
    0.004s

--- week04/Yeram522/yeram522_11399.cpp
+++ week04/Yeram522/yeram522_11399.cpp
@@ -1,6 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-
+//0.004ì´ˆ
 int main()
 {
 	int N,sum=0;
@@ -8,23 +8,23 @@ int main()
 
 	scanf("%d", &N);
 
-	P = new int[N];//µ¿ÀûÇÒ´ç
+	P = new int[N];//ë™ì í• ë‹¹
 
 	for (int i = 0; i < N; i++)
 		scanf("%d", &P[i]);                                                          //ex) 3 1 4 3 2 
 
-	for (int i = N-1 ; 0 <= i; i--)//¹öºíÁ¤·Ä                                        //ex) 1 2 3 3 4
+	for (int i = N-1 ; 0 <= i; i--)//ë²„ë¸”ì •ë ¬                                        //ex) 1 2 3 3 4
 	{
 		for (int j = 0; j < i; j++)
 		{
-			if (P[j] <= P[j + 1]) continue;//j°¡ j+1º¸´Ù Å¬°æ¿ì, µÚ·Î º¸³½´Ù.
+			if (P[j] <= P[j + 1]) continue;//jê°€ j+1ë³´ë‹¤ í´ê²½ìš°, ë’¤ë¡œ ë³´ë‚¸ë‹¤.
 			int temp = P[j + 1];
 			P[j + 1] = P[j];
 			P[j] = temp;
 		}
 	}
 
-	//ÇÊ¿äÇÑ ½Ã°£ÀÇ ÇÕ ÃÖ¼Ú°ª.
+	//í•„ìš”í•œ ì‹œê°„ì˜ í•© ìµœì†Ÿê°’.
 	for (int i = 0; i < N; i++)                            //1         = 1
 	{                                                      //1+2       = 3
 		sum += (N-i) * P[i];                               //1+2+3     = 6
