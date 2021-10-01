7a66729553a45a31d8650b86f09c37fbf1b8a4b2
Marey <63442636+Yeram522@users.noreply.github.com>
2021-10-01 22:22:24 +0900

    [1417] 국회의원 선거
    자꾸 틀렸대서 코드 갈아 엎음! 근데 틀렸대

--- week04/Yeram522/yeram522_1417.cpp
+++ week04/Yeram522/yeram522_1417.cpp
@@ -1,30 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
-
 #include <iostream>
-
-bool IsFirst(int _N,int* _votes)//기호1번이 배열중 최댓값인지 확인한다.
-{
-	for (int i = 1; i < _N; i++)
-	{
-		if (_votes[0] <= _votes[i]) return false;//하나라도 기호1번보다 같거나 많은게 있으면 바로 return false.
-		if (i == _N - 1) return true;
-	}
-}
-
-bool ctex(int _N, int* _votes)
-{
-	for (int i = 1; i < _N; i++)
-	{
-		if (_votes[0] < _votes[i]) return false;//하나라도 기호1번보다 많은게 있으면 바로 return false.
-		if (i == _N - 1) return true;//끝까지 탐색했을 때 1번보다 작거나 같으면 true;
-	}
-}
 
 int main()
 {
-	int N;//N: 총 국회의원 후보 수, M: 마을 주민
+	int N,count=0;//N: 총 국회의원 후보 수
 	int* votes;
-	int buying = 0;
+	int before_vote;
 
 	scanf("%d", &N);
 
@@ -32,24 +13,41 @@ int main()
 
 	for (int i = 0; i < N; i++)
 	{
-		scanf("%d", &votes[i]);
+		scanf("%d", &votes[i]);//입력받으면서 기호1번보다 많으면 	
 	}
 
-	//printf("bool:%d", IsFirst(N, votes));
-
-	while (!IsFirst(N, votes))
+	before_vote = votes[0];
+	int i = 1;//votes[0]은 언제나 기호1번
+	while (i<N)
 	{
-		for (int i = 1; i < N; i++)//votes[0]=다솜(기호1번)
+		if (votes[0] <= votes[i] && votes[i] > 1)
 		{
-			if (IsFirst(N, votes)) break;
-			if (votes[0] >= votes[i] && !ctex(N, votes))continue;//다솜이보다 크거나 같은 득표수를 가질 경우 아래 실행.
+			votes[0]++;
 			votes[i]--;
-			votes[0]++;//상대후보 득표수를 하나 뺏는다.
-			buying++;
 		}
+		else
+			count++;//기호1번이 이긴 사람수
+
+//#ifdef NOT_COMPILE
+		printf("%d", votes[0]);
+		for (int i = 1; i < N; i++)
+		{
+			printf("%d", votes[i]);
+		}
+		printf("\n");
+//#endif
+
+		if (count >= N - 1) break;
+		if (i == N-1 && count < N - 1)
+		{
+			count = 0;
+			i = 0;
+		}
+
+		i++;
 	}
 
-	printf("%d", buying);
+	printf("%d", votes[0]-before_vote);
 
 	return 0;
 }
