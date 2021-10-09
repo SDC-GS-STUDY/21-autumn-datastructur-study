c5e8e37970cdad1b8505ba3e52eb34f36b7b76be
shw619 <67070820+shw619@users.noreply.github.com>
2021-10-09 19:02:09 +0900

    백준 2847번

---
+++ week05/shw619/shw619_2847.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+//배열 뒤부터 생각해서 풀어야됨
+
+int main(void) {
+
+    int n;
+    int a[101];
+    int cnt = 0;
+
+    scanf_s("%d", &n);
+
+    for (int i = 0; i < n; i++)
+        scanf_s("%d", &a[i]);
+
+    //뒤부터 생각해서 앞에 있는 값이 나보다 클 경우는 1씩 감소시켜서 계속 계산시키 ㅁ
+    for (int i = n - 1; i > 0; i--) {
+        if (a[i - 1] >= a[i]) {
+            cnt += a[i - 1] - (a[i] - 1);
+            a[i - 1] = a[i] - 1;
+        }
+    }
+
+   
+
+    printf("%d", cnt);
+
+}
