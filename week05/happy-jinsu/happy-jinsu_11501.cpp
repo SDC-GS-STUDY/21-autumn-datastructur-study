f7ce1964e7b6d68d0d96dd0d224b77482597581b
이수진 <68191793+happy-jinsu@users.noreply.github.com>
2021-10-09 20:53:33 +0900

    11501 주식 완료

--- week05/happy-jinsu/happy-jinsu_11501.cpp
+++ week05/happy-jinsu/happy-jinsu_11501.cpp
@@ -1,45 +1,30 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#define MAX 1000001
+typedef long long ll;
+int StockPrices[1000001] = { 0 };
 
 int main()
 {
-	int i, j, k, T, N, Buy = 0, Max = 0, StockPrices[1000] = { 0 };
+    int i, j, T, N, Buy = 0, Max = 0;
 
-	scanf("%d", &T);
-	for (i = 0; i < T; i++)
-	{
-		Max = 0;
-		Buy = 0;
-		scanf("%d", &N);
-		for (j = 0; j < N; j++)
-		{
-			scanf("%d", &StockPrices[j]);
-			if (StockPrices[j] > Max)
-			{
-				Max = StockPrices[j];
-			}
+    scanf("%d", &T);
+    for (i = 0; i < T; i++) 
+    {
+        scanf("%d", &N); 
+        for (j = 0; j < N; j++)
+            scanf("%d", &StockPrices[j]);
+        
+        Max = StockPrices[N - 1]; 
+        ll Buy = 0;
+        for (j = N - 2; j >= 0; j--)
+        {
+            if (StockPrices[j] > Max)
+                Max = StockPrices[j];
 
-		}
-		for (j = 0; j < N; j++)
-		{
-			if (StockPrices[j] >= Max)
-			{
-				Max = 0;
-				for (k = j+1; k < N; k++)
-				{
-					if (StockPrices[k] > Max)
-					{
-						Max = StockPrices[k];
-					}
-				}
-			}
-			else if (StockPrices[j] < Max)
-			{
-				Buy += Max - StockPrices[j];
-			}
-		}
-		printf("%d\n", Buy);
-	}
-
-	return 0;
+            else if (StockPrices[j] < Max)
+                Buy += ((ll)Max - (ll)StockPrices[j]);
+        }
+        printf("%llu\n", Buy);
+    }
 }
