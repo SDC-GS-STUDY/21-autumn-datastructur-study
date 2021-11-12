3a1e9f2e69045c270d9bc463eb935d13561a749d
Marey <63442636+Yeram522@users.noreply.github.com>
2021-11-13 04:22:52 +0900

    백준 #9095_1,2,3 더하기
    완료

--- week10/Yeram522/yeram522_9095.cpp
+++ week10/Yeram522/yeram522_9095.cpp
@@ -1,43 +1,29 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 
-static int result = 0;
+static int result;
+
 int count(int total)
 {
+	if (total == 0) return result++;
 	
-
-	if (total == 0)
-	{
-		return result++;
-	}
-
-	if (total - 3 >= 0)
-	{
-		count(total - 3);
-	}
-	if (total - 2 >= 0)
-	{
-		count(total - 2);
-	}
-	if (total - 1 >= 0)
-	{
-		count(total - 1);
-	}
-		
+	if (total - 3 >= 0) count(total - 3);	
+	if (total - 2 >= 0) count(total - 2);	
+	if (total - 1 >= 0) count(total - 1);			
 	return result;
 }
 
 int main()
 {
 	int N;
+
 	scanf("%d", &N);
 
 	int* nums = new int[N];
 
-	for (int i = 0; i < N; i++)
-	{
+	for (int i = 0; i < N; i++)	
 		scanf("%d", &nums[i]);
-	}
+	
 
 	for (int i = 0; i < N; i++)
 	{
